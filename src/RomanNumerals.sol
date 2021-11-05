bc9734c71510e69ed291199d73920ebebc2521a4
Chris <chris.chu.yc@gmail.com>
2021-11-04 18:27:26 -0700

    Create RomanNumerals convert function to pass initial test

--- src/RomanNumerals.sol
+++ src/RomanNumerals.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-contract RomanNumeralsKata {
+contract RomanNumerals {
+  function convert(uint) public pure returns (string memory) {
+    return "I";
+  }
 }
