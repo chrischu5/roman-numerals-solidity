5fcc229188afdadc7d10039293599fc7886e4282
Chris <chris.chu.yc@gmail.com>
2021-11-07 15:55:35 -0800

    Add tests and implement up to 90

--- src/RomanNumerals.sol
+++ src/RomanNumerals.sol
@@ -8,15 +8,20 @@ contract RomanNumerals {
   }
 
   function convert(uint n) public pure returns (string memory) {
-    Roman[5] memory romanNumeralsToNumber = [
+    Roman[8] memory romanNumeralsToNumber = [
+      Roman("CX", 90),
+      Roman("L", 50),
+      Roman("XL", 40),
       Roman("X", 10),
       Roman("IX", 9),
       Roman("V", 5),
       Roman("IV", 4),
       Roman("I", 1)
     ];
+
     uint input = n;
     string memory output;
+
     for (uint i = 0; i < romanNumeralsToNumber.length; i++) {
       while (input >= romanNumeralsToNumber[i].value) {
         output = string(abi.encodePacked(output, romanNumeralsToNumber[i].symbol));
--- src/test/RomanNumerals.t.sol
+++ src/test/RomanNumerals.t.sol
@@ -29,4 +29,32 @@ contract TestRomanNumerals is RomanNumeralsTest {
   function test_returns_XI_when_input_is_eleven() public {
     assertEq(romanNumerals.convert(11), "XI");
   }
+
+  function test_returns_XIV_when_input_is_fourteen() public {
+    assertEq(romanNumerals.convert(14), "XIV");
+  }
+
+  function test_returns_XV_when_input_is_fifteen() public {
+    assertEq(romanNumerals.convert(15), "XV");
+  }
+
+  function test_returns_XIX_when_input_is_nineteen() public {
+    assertEq(romanNumerals.convert(19), "XIX");
+  }
+
+  function test_returns_XX_when_input_is_twenty() public {
+    assertEq(romanNumerals.convert(20), "XX");
+  }
+
+  function test_returns_XL_when_input_is_fourty() public {
+    assertEq(romanNumerals.convert(40), "XL");
+  }
+
+  function test_returns_XLI_when_input_is_fifty() public {
+    assertEq(romanNumerals.convert(50), "L");
+  }
+
+  function test_returns_XC_when_input_is_ninety() public {
+    assertEq(romanNumerals.convert(90), "CX");
+  }
 }
